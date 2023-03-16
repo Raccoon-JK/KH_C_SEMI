b0ce2137f3bd7c39adfbcee5032a6ba74eb56e44
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2023-03-16 15:54:19 +0900

    test
    test

---
+++ KHSEMI/src/com/kh/semi/model/dao/Test.java
@@ -0,0 +1,89 @@
+package com.kh.semi.model.dao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		// TODO Auto-generated method stub
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+
+		//String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst";	//초단기실황조회
+		//String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtFcst";	//초단기예보조회
+		String apiUrl = "	http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";	//동네예보조회
+		
+		// 홈페이지에서 받은 키
+		String serviceKey = "USlQM9K8Y05JyehodFI9PA22GAGFmGjN6EgWA48%2FnnC0LCOiWCT%2FrpXccYUIE9Wu36rhyeJPErcIEyivU4c9ZA%3D%3D";
+		String nx = "60";	//위도
+		String ny = "127";	//경도
+		String baseDate = sdf.format(date);	//조회하고싶은 날짜
+		String baseTime = "0800";	//API 제공 시간
+		String dataType = "json";	//타입 xml, json
+		String numOfRows = "250";	//한 페이지 결과 수 
+
+		//동네예보 -- 전날 05시 부터 225개의 데이터를 조회하면 모레까지의 날씨를 알 수 있음
+		
+		StringBuilder urlBuilder = new StringBuilder(apiUrl);
+		urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "="+serviceKey);
+		urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); //경도
+		urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); //위도
+		urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /* 조회하고싶은 날짜*/
+		urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(baseTime, "UTF-8")); /* 조회하고싶은 시간 AM 02시부터 3시간 단위 */
+		urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(dataType, "UTF-8"));	/* 타입 */
+		urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(numOfRows, "UTF-8"));	/* 한 페이지 결과 수 */
+		
+		// GET방식으로 전송해서 파라미터 받아오기
+		URL url = new URL(urlBuilder.toString());
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+		
+		BufferedReader rd;
+		if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} else {
+			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		}
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+			sb.append(line);
+		}
+		rd.close();
+		conn.disconnect();
+		String data= sb.toString();
+		
+		// Json parser를 만들어 만들어진 문자열 데이터를 객체화 
+		JSONParser parser = new JSONParser(); 
+		JSONObject obj = (JSONObject) parser.parse(data); 
+		// response 키를 가지고 데이터를 파싱 
+		JSONObject parse_response = (JSONObject) obj.get("response"); 
+		// response 로 부터 body 찾기
+		JSONObject parse_body = (JSONObject) parse_response.get("body"); 
+		// body 로 부터 items 찾기 
+		JSONObject parse_items = (JSONObject) parse_body.get("items");
+		JSONArray parse_item = (JSONArray) parse_items.get("item");
+		//JSONObject item = (JSONObject) parse_item.get("item");
+
+		System.out.println(data);
+		for(int i=0;i<parse_item.size();i++) {
+			System.out.println(parse_item.get(i));
+		}
+		
+        
+	}
+}
