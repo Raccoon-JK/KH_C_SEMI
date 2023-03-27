d540dc55b5fa9880288e13dfa85ea73bf9f0570f
SardineSP <5039662@naver.com>
2023-03-27 14:48:59 +0900

    Update raining.js

--- KHSEMI/WebContent/views/common/raining.js
+++ KHSEMI/WebContent/views/common/raining.js
@@ -7,17 +7,17 @@ function rain() {
             let body = document.querySelector("body");
             let count = 0;
             while (count < amount) {
-            let drop = document.createElement("i");
-            let size = Math.random() * 3;
-            let posX = Math.floor(Math.random() * window.innerWidth);
-            let delay = Math.random() * -10;
-            let duration = Math.random() * 1;
-            drop.style.width = `${0.1 + size}px`;
-            drop.style.left = `${posX}px`;
-            drop.style.animationDelay = `${delay}s`;
-            drop.style.animationDuration = `${1 + duration}s`;
-            body.appendChild(drop);
-            count++;
+            	let drop = document.createElement("i");
+            	let size = Math.random() * 3;
+            	let posX = Math.floor(Math.random() * window.innerWidth);
+            	let delay = Math.random() * -10;
+            	let duration = Math.random() * 1;
+            	drop.style.width = `${0.1 + size}px`;
+            	drop.style.left = `${posX}px`;
+            	drop.style.animationDelay = `${delay}s`;
+            	drop.style.animationDuration = `${1 + duration}s`;
+            	body.appendChild(drop);
+            	count++;
             }
-            }
-            rain();
+        }
+        rain();
